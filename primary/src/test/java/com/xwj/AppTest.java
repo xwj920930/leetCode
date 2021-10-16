33b1820ca2b96488b3f371910989ce5ed7c0d647
xuwenjie <xuwenjie@travelsky.com>
2021-10-16 18:19:32 +0800

    update

--- primary/src/test/java/com/xwj/AppTest.java
+++ primary/src/test/java/com/xwj/AppTest.java
@@ -2,19 +2,23 @@ package com.xwj;
 
 import static org.junit.Assert.assertTrue;
 
+import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        TestCase.assertTrue( true );
     }
 }
